75aaea09ab50d5c88faefc03051b2d57642261e2
Miguel S. Ramos <miguelsramos458@gmail.com>
2022-12-24 15:53:37 -0300

    :sparkles:	feat: Adiciona UpdateUserController

--- src/controllers/update-user/protocols.ts
+++ src/controllers/update-user/protocols.ts
@@ -1,9 +1,14 @@
 import { User } from "../../models/user";
+import { IHttpRequest, IHttpResponse } from "../protocols";
 
 export interface IUpdateUserParams {
   firstName?: string;
   lastName?: string;
   password?: string;
+}
+
+export interface IUpdateUserController {
+  handle(httpRequest: IHttpRequest<any>): Promise<IHttpResponse<User>>
 }
 
 export interface IUpdateUserRepository {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/create-user/create-user";
 import { GetUsersController } from "../controllers/get-users/get-users";
+import { UpdateUserController } from "../controllers/update-user/update-user";
 import { MongoCreateUserRepository } from "../repositories/create-user/mongo-create-user";
 import { MongoGetUsersRepository } from "../repositories/get-users/mongo-get-users";
+import { MongoUpdateUserRepository } from "../repositories/update-user/mongo-update-user";
 const routes = Router();
 
 routes.get("/users", async (req, res) => {
@@ -19,6 +21,18 @@ routes.post("/users", async (req, res) => {
   );
   const { body, statusCode } = await createUsersController.handle({
     body: req.body,
+  });
+  res.status(statusCode).send(body);
+});
+
+routes.patch("/users/:id", async (req, res) => {
+  const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+  const updateUserController = new UpdateUserController(
+    mongoUpdateUserRepository
+  );
+  const { body, statusCode } = await updateUserController.handle({
+    body: req.body,
+    params: req.params,
   });
   res.status(statusCode).send(body);
 });
