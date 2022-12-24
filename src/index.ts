8fef04c2534c5f55e386a53732087d8f4aa8b2c6
Miguel S. Ramos <miguelsramos458@gmail.com>
2022-12-24 10:40:40 -0300

    :sparkles:	feat: Criando server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express from "express";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8089;
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
+
+app.get("/api/v1", (req, res) => {
+  res.send("hello world!");
+});
