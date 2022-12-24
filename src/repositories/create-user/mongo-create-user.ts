40ea6d93cca199b3a4f19f6326238b65177eab23
Miguel S. Ramos <miguelsramos458@gmail.com>
2022-12-24 13:07:20 -0300

    :sparkles:	feat: Adiciona MongoCreateUserRepository

---
+++ src/repositories/create-user/mongo-create-user.ts
@@ -0,0 +1,25 @@
+import {
+  ICreateUserParams,
+  ICreateUserRepository,
+} from "../../controllers/create-user/protocols";
+import { MongoClient } from "../../database/mongo";
+import { User } from "../../models/user";
+
+export class MongoCreateUser implements ICreateUserRepository {
+  async createUser(params: ICreateUserParams): Promise<User> {
+    const { insertedId } = await MongoClient.db
+      .collection("users")
+      .insertOne(params);
+
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .findOne({ _id: insertedId });
+
+    if (!user) {
+      throw new Error("User not createrd");
+    }
+
+    const { _id, ...rest } = user;
+    return { id: _id.toHexString(), ...rest };
+  }
+}
